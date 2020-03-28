74fbe3586b6358e02ada1e03336e5c24f563252d
danomov <dx555r@gmail.com>
2020-03-28 12:47:46 +0400

    make corrections and clean up

--- topVirusCasesChart.js
+++ topVirusCasesChart.js
@@ -1,74 +1,69 @@
 Chart.platform.disableCSSInjection = true;
 
-let xAxis = [], yAxis = [];
+function handleTopVirusCountryChart() {
+    let countries = [], activeCases = [];
 
-const fetchData = async () => {
-    await fetch("https://covid-193.p.rapidapi.com/statistics", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "covid-193.p.rapidapi.com",
-            "x-rapidapi-key": "98061a671bmsh02ad22d36807bf3p113143jsn8aa932aabf70"
-        }
-    })
-        .then(response => {
-            return response.json()
+    const fetchData = async () => {
+        await fetch("https://covid-193.p.rapidapi.com/statistics", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "covid-193.p.rapidapi.com",
+                "x-rapidapi-key": "98061a671bmsh02ad22d36807bf3p113143jsn8aa932aabf70"
+            }
         })
-        .then(apiData => {
-            return sortData(apiData.response)
-        })
-        .then(sorted => {
-            fillData(sorted);
-        })
-        .catch(err => {
-            console.log(err);
+            .then(response => {
+                return response.json()
+            })
+            .then(apiData => {
+                return sortData(apiData.response)
+            })
+            .then(sorted => {
+                fillData(sorted);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
+
+    const sortData = (data) => {
+        const sortedData = data.sort((a, b) => {
+            return b.cases.active - a.cases.active
         });
-};
 
-const sortData = (data) => {
-    const sortedData = data.sort((a, b) => {
-        return b.cases.active - a.cases.active
-    });
+        return sortedData
+    }
 
-    return sortedData
+    const fillData = async (data) => {
+        await data.slice(0, 70).forEach(element => {
+            if (element.country !== 'All') {
+                countries.push(element.country);
+                activeCases.push(element.cases.active)
+            }
+        });
+
+        drawChart()
+    }
+
+    const drawChart = () => {
+        const cnvs = document.getElementById("virusChartTopCountries").getContext("2d");
+        const virusChart = new Chart(cnvs, {
+            type: "bar",
+            data: {
+                labels: countries,
+                datasets: [
+                    {
+                        label: "# of Active Cases",
+                        data: activeCases,
+                        backgroundColor: "rgba(255, 99, 132, 0.2)",
+                        borderColor: "rgba(255, 99, 132, 1)",
+                        borderWidth: 1
+                    }
+                ]
+            }
+        });
+    };
+
+    fetchData();
 }
 
-const fillData = async (data) => {
-    await data.slice(0, 70).forEach(selection => {
-        if (selection.country !== 'All') {
-            xAxis.push(selection.country);
-            yAxis.push(selection.cases.active)
-        }
-    });
-
-    drawChart()
-}
-
-const drawChart = () => {
-    const cnvs = document.getElementById("virusChartTop70").getContext("2d");
-    const myChart = new Chart(cnvs, {
-        type: "bar",
-        data: {
-            labels: xAxis,
-            datasets: [
-                {
-                    label: "# of Active Cases",
-                    data: yAxis,
-                    backgroundColor: "rgba(255, 99, 132, 0.2)",
-                    borderColor: "rgba(255, 99, 132, 1)",
-                    borderWidth: 1
-                }
-            ]
-        }
-        // options: {
-        //     scales: {
-        //         yAxes: [{
-        //             ticks: {
-        //                 beginAtZero: true
-        //             }
-        //         }]
-        //     }
-        // }
-    });
-};
-
-fetchData();
+handleTopVirusCountryChart();
--- virusCasesPerPopulation.js
+++ virusCasesPerPopulation.js
@@ -1,104 +1,97 @@
-let xAxis1 = [], yAxis1 = [], sortedCases = [], populationArr = [], sortedCasesNum = [];
+function handleVirusCasePerPopPercentageChart() {
+    let countries = [], percentage = [], sortedCases = [], populationQuantity = [], sortedCaseQuantity = [];
 
-const fetchData1 = async () => {
-    await fetch("https://covid-193.p.rapidapi.com/statistics", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "covid-193.p.rapidapi.com",
-            "x-rapidapi-key": "98061a671bmsh02ad22d36807bf3p113143jsn8aa932aabf70"
-        }
-    })
-        .then(response => {
-            return response.json()
-        })
-        .then(apiData => {
-            sortData1(apiData.response)
-        })
-        .then(fetchPopulation)
-        .catch(err => {
-            console.log(err);
-        });
-};
-
-const requestPopInfo = async (countryName) => {
-    return await fetch(`https://restcountries-v1.p.rapidapi.com/name/${countryName}`, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
-            "x-rapidapi-key": "98061a671bmsh02ad22d36807bf3p113143jsn8aa932aabf70"
-        }
-    })
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            return data[0] ? data[0].population : null;
-        })
-        .catch(err => {
-            console.log(err);
-        });
-}
-
-const fetchPopulation = async () => {
-    await new Promise(res => {
-        sortedCases.forEach(async function (cases, index) {
-            console.log(cases)
-            if (cases.country !== 'All') {
-                const population = await requestPopInfo(cases.country);
-                populationArr.push(population);
-                sortedCasesNum.push(cases.cases.active);
-                xAxis1.push(cases.country);
-            }
-            if (index === sortedCases.length - 1) {
-                res()
+    const fetchData = async () => {
+        await fetch("https://covid-193.p.rapidapi.com/statistics", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "covid-193.p.rapidapi.com",
+                "x-rapidapi-key": "98061a671bmsh02ad22d36807bf3p113143jsn8aa932aabf70"
             }
         })
+            .then(response => {
+                return response.json()
+            })
+            .then(apiData => {
+                sortData(apiData.response)
+            })
+            .then(fetchPopulation)
+            .catch(err => {
+                console.log(err);
+            });
+    };
 
-    }).then(makeMath)
-}
+    const requestPopInfo = async (countryName) => {
+        return await fetch(`https://restcountries-v1.p.rapidapi.com/name/${countryName}`, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
+                "x-rapidapi-key": "98061a671bmsh02ad22d36807bf3p113143jsn8aa932aabf70"
+            }
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                return data[0] ? data[0].population : null;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
 
-const makeMath = async () => {
-    console.log(populationArr, sortedCasesNum)
-    await populationArr.forEach((el, index) => {
-        el !== null ? yAxis1.push(sortedCasesNum[index] / el) : yAxis1.push(0)
-    })
-
-    console.log(yAxis1)
-    drawChart1()
-}
-
-const sortData1 = (data) => {
-    sortedCases = data.sort((a, b) => {
-        return b.cases.active - a.cases.active
-    });
-}
-
-const drawChart1 = () => {
-    const cnvs = document.getElementById("virusChartPerPopulation").getContext("2d");
-    const myChart = new Chart(cnvs, {
-        type: "bar",
-        data: {
-            labels: xAxis1,
-            datasets: [
-                {
-                    label: "# of Active Cases/Population",
-                    data: yAxis1,
-                    backgroundColor: "rgba(255, 99, 132, 0.2)",
-                    borderColor: "rgba(255, 99, 132, 1)",
-                    borderWidth: 1
+    const fetchPopulation = async () => {
+        await new Promise(res => {
+            sortedCases.forEach(async function (cases, index) {
+                if (cases.country !== 'All') {
+                    const population = await requestPopInfo(cases.country);
+                    populationQuantity.push(population);
+                    sortedCaseQuantity.push(cases.cases.active);
+                    countries.push(cases.country);
                 }
-            ]
-        }
-        // options: {
-        //     scales: {
-        //         yAxes: [{
-        //             ticks: {
-        //                 beginAtZero: true
-        //             }
-        //         }]
-        //     }
-        // }
-    });
-};
+                if (index === sortedCases.length - 1) {
+                    res()
+                }
+            })
 
-fetchData1();
+        }).then(makeMath)
+    }
+
+    const makeMath = async () => {
+        populationQuantity.forEach((population, index) => {
+            population !== null ? percentage.push(sortedCaseQuantity[index] / population) : percentage.push(0)
+        })
+
+        drawChart()
+    }
+
+    const sortData = (data) => {
+        sortedCases = data.sort((a, b) => {
+            return b.cases.active - a.cases.active
+        });
+    }
+
+    const drawChart = () => {
+        const cnvs = document.getElementById("virusChartPerPopulation").getContext("2d");
+        const virusChart = new Chart(cnvs, {
+            type: "bar",
+            data: {
+                labels: countries,
+                datasets: [
+                    {
+                        label: "# of Active Cases/Population",
+                        data: percentage,
+                        backgroundColor: "rgba(255, 99, 132, 0.2)",
+                        borderColor: "rgba(255, 99, 132, 1)",
+                        borderWidth: 1
+                    }
+                ]
+            }
+        });
+    };
+
+    fetchData();
+}
+
+
+handleVirusCasePerPopPercentageChart()
